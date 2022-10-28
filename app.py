3be0a2c460f99b4fe1e0c3f7d146971a2432fee7
achook <karol@kucza.xyz>
2022-10-28 13:51:25 +0200

    Add health check, cleanup

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask
 
-from db import connect
+from health import check_health
 
 app = Flask(__name__)
-
-connect()
 
 
 @app.route("/")
 def hello_world():
     return "Hello, world!"
+
+
+@app.route("/health")
+def health():
+    status, message = check_health()
+    if status:
+        return {
+            "status": "ok",
+            "message": None
+        }
+    else:
+        return {
+            "status": "error",
+            "message": message
+        }
---
+++ health.py
@@ -0,0 +1,10 @@
+def check_health() -> [bool, str]:
+    """
+    Checks health of the whole backend system
+
+    Returns:
+        [bool, str]: [True, None] if everything is ok, [False, str] if there is an error.
+        The string is a human-readable error message.
+
+    """
+    return True, None
