2ad01e29c8250f230bf55bf8d1b4897e008efa83
achook <karol@kucza.xyz>
2022-11-05 00:12:49 +0100

    Remove the Conversation class
    Removed the Conversation class, moved logic to Message

--- messages.py
+++ messages.py
@@ -15,6 +15,11 @@ class Message:
     def send_message(self) -> None:
         return
 
+    # TODO: Implement, should get all user's messages
+    @classmethod
+    def get_messages_by_user_id(cls, user_id: str) -> list["Message"]:
+        return []
+
     # TODO: Implement, see https://stackoverflow.com/questions/1436703/what-is-the-difference-between-str-and-repr
     def __str__(self):
         pass
@@ -22,29 +27,3 @@ class Message:
     # TODO: Implement, see https://stackoverflow.com/questions/1436703/what-is-the-difference-between-str-and-repr
     def __repr__(self):
         pass
-
-
-@dataclass(init=True, eq=True, order=True, unsafe_hash=False, frozen=False)
-class Conversation:
-    conversation_id: str
-    messages: list[Message]
-
-    # TODO: Implement, should get a list of conversations for a user
-    #  Should raise an exception if the operation fails
-    @classmethod
-    def get_conversations_by_user_id(cls, user_id: str) -> list["Conversation"]:
-        return []
-
-    # TODO: Implement, should get a single conversations for an id
-    #  Should raise an exception if the operation fails or if the conversation does not exist
-    @classmethod
-    def get_conversation_by_id(cls, conversation_id: str) -> "Conversation":
-        return Conversation("", [])
-
-    # TODO: Implement, see https://stackoverflow.com/questions/1436703/what-is-the-difference-between-str-and-repr
-    def __str__(self) -> str:
-        return ""
-
-    # TODO: Implement, see https://stackoverflow.com/questions/1436703/what-is-the-difference-between-str-and-repr
-    def __repr__(self) -> str:
-        return ""
