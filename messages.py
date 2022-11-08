31e403852dcca684018f3636035fc1aad68e00e9
achook <karol@kucza.xyz>
2022-11-08 12:55:32 +0100

    Add methods for getting and sending messages

--- messages.py
+++ messages.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from users import User
 from dataclasses import dataclass
+from db import execute, fetch
 
 
 @dataclass(init=True, eq=True, order=True, unsafe_hash=False, frozen=False)
@@ -11,14 +12,21 @@ class Message:
     content: str
     sent_at: datetime
 
-    # TODO: Implement, should send a message (put it in the database) and raise an exception if it fails
     def send_message(self) -> None:
-        return
+        result = execute("INSERT INTO messages (sender_id, receiver_id, content, sent_at) VALUES (%s, %s, %s, %s)",
+                         (self.sender.uid, self.receiver.uid, self.content, self.sent_at))
 
-    # TODO: Implement, should get all user's messages
     @classmethod
     def get_messages_by_user_id(cls, user_id: str) -> list["Message"]:
-        return []
+        m = []
+
+        result = fetch("SELECT * FROM messages WHERE sender_id = %s OR receiver_id = %s", (user_id, user_id))
+        for raw_message in result:
+            sender = User.get_user_by_id(raw_message[1])
+            receiver = User.get_user_by_id(raw_message[2])
+            m.append(cls(raw_message[0], sender, receiver, raw_message[3], raw_message[4]))
+
+        return m
 
     # TODO: Implement, see https://stackoverflow.com/questions/1436703/what-is-the-difference-between-str-and-repr
     def __str__(self):
