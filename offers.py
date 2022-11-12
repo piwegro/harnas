c01841717b5c14808c66d71dd23ba5599ccc07ff
achook <karol@kucza.xyz>
2022-11-12 14:23:20 +0100

    Add adding offers
    Adds functions for handling adding offers

--- offers.py
+++ offers.py
@@ -3,17 +3,18 @@ from datetime import datetime
 from dataclasses import dataclass
 from users import User
 from currencies import Currency, Price
+from typing import Optional
 
 # Exceptions
-from exc import OfferNotFoundError
+from exc import OfferNotFoundError, UserNotFoundError, CurrencyNotFoundError
 
 # Functions import
-from db import fetch
+from db import fetch, execute
 
 
 @dataclass(init=True, eq=True, order=True, unsafe_hash=False, frozen=False)
 class Offer:
-    id: str
+    id: Optional[str]
     title: str
     description: str
     price: Price
@@ -21,16 +22,70 @@ class Offer:
     images: list[str]
     created_at: datetime
 
-    # TODO: Should add an offer to the database and then return the offer
+    is_added = property(lambda self: self.id is not None)
+
     @classmethod
     def new_offer(cls, title: str, description: str, price: Price, seller: User, images: list[str]) -> "Offer":
-        return cls("", "", "", Price(0, Currency("", "", 1.0)), User("", "", "", []), [], datetime.now())
+        """
+        Creates a new offer.
+
+        :param title: The title of the offer.
+        :param description: The description of the offer.
+        :param price: The price of the offer.
+        :param seller: The seller.
+        :param images: The list of links to the images.
+        :return: The new offer.
+        """
+        return cls(None, title, description, price, seller, images, datetime.now())
+
+    @classmethod
+    def new_offer_with_id(cls, title: str, description: str, price: Price, seller_id: str, images: list[str]) -> "Offer":
+        """
+        Creates a new offer, but with seller id instead of seller object.
+
+        :param title: The title of the offer.
+        :param description: The description of the offer.
+        :param price: The price of the offer.
+        :param seller_id: The id of the seller.
+        :param images: The list of links to the images.
+        :return: The new offer.
+        """
+        try:
+            seller = User.get_user_by_id(seller_id)
+        except UserNotFoundError:
+            raise
+
+        return cls(None, title, description, price, seller, images, datetime.now())
 
     @classmethod
     def new_offer_from_row(cls, raw_offer) -> "Offer":
+        try:
+            currency = Currency.get_currency_by_symbol(raw_offer[4])
+            user = User.get_user_by_id(raw_offer[1])
+        except UserNotFoundError:
+            raise
+        except CurrencyNotFoundError:
+            raise
+
         return cls(raw_offer[0], raw_offer[2], raw_offer[3],
-                   Price(raw_offer[4], Currency.get_currency_by_symbol(raw_offer[5])),
-                   User.get_user_by_id(raw_offer[1]), raw_offer[6], raw_offer[7])
+                   Price(raw_offer[4], currency),
+                   user, raw_offer[6], raw_offer[7])
+
+    def add(self) -> None:
+        """
+        Adds the offer to the database.
+        """
+        execute("INSERT INTO offers (seller_id, title, description, price, currency, images, created_at) "
+                "VALUES (?, ?, ?, ?, ?, ?, ?)",
+                (self.seller.uid, self.title, self.description, self.price.amount, self.price.currency.symbol,
+                 self.images, self.created_at))
+
+        result = fetch("SELECT id FROM offers WHERE seller_id = ? AND title = ? AND description = ?"
+                       "AND price = ? AND currency = ? AND images = ? AND created_at = ?",
+                       (self.seller.uid, self.title, self.description, self.price.amount,
+                        self.price.currency.symbol, self.images, self.created_at))
+
+        self.id = result[0][0]
 
     @classmethod
     def get_all_offers(cls) -> list["Offer"]:
