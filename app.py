b7d062ea166874388e39d5b4af6741e6455be5e7
achook <karol@kucza.xyz>
2022-11-18 00:15:25 +0100

    Hotfix

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def handle_get_offers_by_query(query: str, page: str):
         offers = Offer.search_offers(query, int(page))
     except Exception as e:
         print("Exception:", e)
-        return Error("Internal server error").to_json(), 500
+        return Error("Internal server error").to_json(500)
 
     return offers, 200
 
